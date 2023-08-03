040c7770f5bbf9eb1d872ebdce7407ecb0fb0c85
Mrinal Nath <nath.mrinal@gmail.com>
2023-08-03 05:38:56 +0530

    updated

---
+++ sonar-demo-module1/src/main/java/com/example/sonardemomodule1/SonarDemoModule1Application.java
@@ -0,0 +1,13 @@
+package com.example.sonardemomodule1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarDemoModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarDemoModule1Application.class, args);
+	}
+
+}
---
+++ sonar-demo-module1/src/test/java/com/example/sonardemomodule1/SonarDemoModule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonardemomodule1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarDemoModule1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
